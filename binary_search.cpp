a6f94cf309a82be501c19e1a5927e32a350f3704
BigCatDi <smarthwd@163.com>
2020-07-13 00:43:33 +0800

    2020.07.13modify

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
+#include <vector>
 //二分查找（循环）
-bool binary_search(sed:vector<int> &sort array, int target){
+bool binary_search(std::vector<int> &sort_array, int target){
     int begin=0;
-    int end=array.size()-1;
+    int end=sort_array.size()-1;
     while(begin<end){
         int mid=(begin+end)/2;
-        if(target==sort array[mid])
+        if(target==sort_array[mid])
             return true;
-        else if(target<sort array[mid])
+        else if(target<sort_array[mid])
             end=mid-1;
-        else if(target>sort array[mid])
+        else if(target>sort_array[mid])
             begin=mid+1; 
     }
         return false;
@@ -18,3 +18,15 @@ bool binary_search(sed:vector<int> &sort array, int target){
 
 
 //二分查找（递归）
+bool binary_search(std::vector<int> &sort_array, int begin ,int end,int target){
+    if(begin > end)
+        return false;
+    int mid=(begin+end)/2;
+    if(target ==sort_array[mid])
+        return true;
+    else if(target<sort_array[mid])
+        return binary_search(sort_array, begin, mid-1, target);
+    else if(target>sort_array[mid])
+        return binary_search(sort_array, mid+1, end, target);
+
+}
